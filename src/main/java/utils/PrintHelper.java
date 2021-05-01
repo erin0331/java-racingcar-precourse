97cbeac66bd1031bbc8eb5a13fdd84d1804cc3f8
Eunjeong Choi <ldmswjdl@naver.com>
2021-05-02 03:12:44 +0900

    feat : Message enum추가, 게임진행 중 출력 메시지 메소드 추가

--- src/main/java/utils/PrintHelper.java
+++ src/main/java/utils/PrintHelper.java
@@ -5,6 +5,31 @@ import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 
 public class PrintHelper {
+	private enum Message {
+		INPUT_CARS("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+		INPUT_ROUND("시도할 회수는 몇회인가요?"),
+		GUIDANCE("실행 결과"),
+		RESULT("(이)가 최종 우승했습니다.");
+		private String message;
+
+		Message(String message) {
+			this.message = message;
+		}
+	}
+
+	public static void printInputCars() {
+		System.out.println(Message.INPUT_CARS.message);
+	}
+
+	public static void printInputRound() {
+		System.out.println(Message.INPUT_ROUND.message);
+	}
+
+	public static void printGuidance() {
+		printLine();
+		System.out.println(Message.GUIDANCE.message);
+	}
+
 	public static void printScoreStatus(String name, int score) {
 		System.out.print(StringUtils.join(name, " : ", printBar(score)));
 		printLine();
@@ -28,7 +53,7 @@ public class PrintHelper {
 		for (int i = 0; i < winnerCount; i++) {
 			resultMessage = StringUtils.join(resultMessage, winnerNames.get(i), addComma(winnerCount, i));
 		}
-		System.out.println(StringUtils.join(resultMessage, "(이)가 최종 우승했습니다."));
+		System.out.println(StringUtils.join(resultMessage, Message.RESULT.message));
 	}
 
 	private static String addComma(int winnerCount, int now) {
