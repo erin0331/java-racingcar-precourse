0700e9f2fc2c08ebab7140aba147315a918f5958
Eunjeong Choi <ldmswjdl@naver.com>
2021-05-04 00:33:44 +0900

    refactor : Judgement를 인스턴스 변수로 변경, Cars에서 Judgement를 활용하여 play하도록 변경

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,16 +5,18 @@ public class Game {
 
 	private final Cars cars;
 	private final int roundCount;
+	private final Judgement judgement;
 
 	public Game(Cars cars, String roundCount) {
 		this.cars = cars;
 		this.roundCount = Validator.getValidRoundCount(roundCount);
+		this.judgement = new Judgement();
 	}
 
 	public void play() {
 		PrintHelper.printGuidance();
 		for (int i = 0; i < roundCount; i++) {
-			cars.play();
+			cars.play(judgement);
 			cars.printScore();
 		}
 		printResult();
